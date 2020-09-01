bfc9eefc53957ef36e47921cd1f1e7c0612ec693
LazzaAU <larry@fulltimecaravanning.com.au>
2020-09-01 14:49:38 +1000

    Create a Alice backup Skill

--- BackUpAlice.py
+++ BackUpAlice.py
@@ -1,3 +1,8 @@
+import subprocess
+import os
+import shutil
+from datetime import date, timedelta
+from pathlib import Path
 from core.base.model.AliceSkill import AliceSkill
 from core.dialog.model.DialogSession import DialogSession
 from core.util.Decorators import IntentHandler
@@ -9,6 +14,106 @@ class BackUpAlice(AliceSkill):
 	Description: Makes a backup copy of the projectalice folder
 	"""
 
-	@IntentHandler('MyIntentName')
-	def dummyIntent(self, session: DialogSession, **_kwargs):
-		pass
+
+	def __init__(self):
+		self._dateFormat = "%b-%d"
+		self._backupCopy = Path
+		self._monthAndDate = ""
+		super().__init__()
+
+
+	@IntentHandler('BackUpAlice')
+	def backupProjectAlice(self, session: DialogSession = None):
+
+		mainPath = Path(f'{self.homeDir()}/AliceBackUp')
+
+		# if ere's no AliceBackUp directory then make one
+		if not mainPath.exists():
+			mainPath.mkdir()
+
+		backupDirectory = os.listdir(mainPath)
+
+		# Checking if the AliceBackUp directory is empty or not
+		if len(backupDirectory) == 0:
+			self.preChecks()
+			if session:
+				self.endDialog(
+					sessionId=session.sessionId,
+					text='I\'m making your first back up now.. Yay',
+					siteId=session.siteId
+				)
+
+			self.ThreadManager.doLater(
+				interval=6,
+				func=self.runCopyCommand
+			)
+		else:
+			if session:
+				self.backupChecks(session)
+			else:
+				self.backupChecks()
+
+
+	def preChecks(self):
+		today = date.today()
+		self._monthAndDate = today.strftime(self._dateFormat)
+		self._backupCopy = Path(f'{self.homeDir()}/AliceBackUp/ProjectAlice-{self._monthAndDate}')
+
+
+	def backupChecks(self, session = None):
+		backupDuration = self.getConfig('DaysBetweenBackups')
+		# determine the date N days ago
+		dateNdaysAgo = date.today() - timedelta(days=backupDuration)
+
+		today = date.today().strftime(self._dateFormat)
+		previous = dateNdaysAgo.strftime(self._dateFormat)
+
+		# Does our previous back up match our backup duration ?
+		expiredPreviousBackup = Path(f'{self.homeDir()}/AliceBackUp/ProjectAlice-{previous}')
+		backUpPath = f'{self.homeDir()}/AliceBackUp'
+
+		if expiredPreviousBackup.exists():
+			print(f'yes file exists, delete it and make a new one')
+			print(f'date today {today}')
+			print(f' date {backupDuration} days ago {previous}')
+			shutil.rmtree(backUpPath, ignore_errors=False, onerror=None)
+			Path(self.homeDir(), 'AliceBackUp').mkdir()
+			self.preChecks()
+
+			if session:
+				self.endDialog(
+					sessionId=session.sessionId,
+					text='Renewing your back up now',
+					siteId=session.siteId
+				)
+			self.logInfo(f'Renwing your saved back up file')
+			self.ThreadManager.doLater(
+				interval=6,
+				func=self.runCopyCommand
+			)
+		else:
+			if session:
+				self.endDialog(
+					sessionId=session.sessionId,
+					text='There\'s nothing to update. you\'re up to date on your updates. giggles',
+					siteId=session.siteId
+				)
+			self.logInfo(f'Current backups are up to date, no further action taken')
+
+
+	def runCopyCommand(self):
+		subprocess.run(['cp', '-R', self.rootDir(), self._backupCopy])
+
+
+	def onFullHour(self):
+		self.backupProjectAlice()
+
+
+	@staticmethod
+	def homeDir() -> str:
+		return str(Path(__file__).resolve().parent.parent.parent.parent)
+
+
+	@staticmethod
+	def rootDir() -> str:
+		return str(Path(__file__).resolve().parent.parent.parent)
